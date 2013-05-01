140b59a6b62d8dbba9060a3b1063c3441f8b1ab0
FScoward <FScoward@FSCOWARD-PC>
2013-05-02 01:14:16 +0900

    2013/05/02

---
+++ src/fscoward/view/spherepuyopuyo/Puyo.java
@@ -0,0 +1,15 @@
+package fscoward.view.spherepuyopuyo;
+
+interface Puyo {
+	enum Sphere {HARUKA, MINAKO, AYAHI, AKI};
+	
+	// ˆÚ“®“®
+	public void move();
+	// ‰ñ“]
+	public void rotate();
+	//@Á–Å
+	public void eliminate();
+	
+	String color = null;
+	
+}
